f0b1f06f569fe938581e8b1a2a28f3863326e1a3
Young2218 <44011517+Young2218@users.noreply.github.com>
2021-05-19 22:49:40 +0900

    포스팅 목록 읽기
    포스팅 목록 읽기 기능 구현
    아직 포스트에 날짜랑 좋아요 댓글 읽는건 구현 못했음

--- cosu/app/src/main/java/com/example/cosu_pra/Main/SearchActivity.java
+++ cosu/app/src/main/java/com/example/cosu_pra/Main/SearchActivity.java
@@ -1,38 +1,77 @@
 package com.example.cosu_pra.Main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.example.cosu_pra.ConnectFB.HelpPosting;
+import com.example.cosu_pra.DTO.ProjectPost;
 import com.example.cosu_pra.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SearchActivity extends AppCompatActivity {
 
-
+    HelpPosting postHelper;
     ListView listView;
-    StudyAdapter adapter ;
+    StudyAdapter adapter;
     StudyItem studyItem;
+    ArrayList<String> postIDs = new ArrayList<>();
+    ArrayList<ProjectPost> posts = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        listView =(ListView) findViewById(R.id.listview);
+        postHelper = new HelpPosting();
+        listView = (ListView) findViewById(R.id.listview);
 
-        adapter = new StudyAdapter() ;
+        adapter = new StudyAdapter();
         listView.setAdapter(adapter);
 
-        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.android),
-                "안드로이드" , "5", "~2021/5/31","3","5") ;
-        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.java),
-                "자바" , "6", "~2021/5/31","4","6") ;
-        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.kotlin),
-                "코틀린" , "3", "~2021/5/31","1","0") ;
-        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.python),
-                "파이썬" , "7", "~2021/5/31","2","2") ;
+        postHelper.getAllPosts(HelpPosting.PROJECT).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        postIDs.add(document.getId());
+                        posts.add(document.toObject(ProjectPost.class));
+                    }
+                    for (ProjectPost pp : posts) {
+                        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.android),
+                                pp.getTitle(), pp.getMax()+"", "~2021/5/31", "3", "5");
+                    }
+                    listView.setAdapter(adapter);
+                }
+            }
+        });
 
+
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.android),
+//                "안드로이드", "5", "~2021/5/31", "3", "5");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.java),
+//                "자바", "6", "~2021/5/31", "4", "6");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.kotlin),
+//                "코틀린", "3", "~2021/5/31", "1", "0");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.python),
+//                "파이썬", "7", "~2021/5/31", "2", "2");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
