487a8ec48fcfff3f4609d590ddf3a055092123dc
Young2218 <44011517+Young2218@users.noreply.github.com>
2021-05-21 17:41:16 +0900

    포스팅 수정
    스피너 선택시 선택된 카테고리만 나오게 수정

--- cosu/app/src/main/java/com/example/cosu_pra/Main/SearchActivity.java
+++ cosu/app/src/main/java/com/example/cosu_pra/Main/SearchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +33,8 @@ public class SearchActivity extends AppCompatActivity {
     PostListItem studyItem;
     Map<String, ProjectPost> projectPosts = new HashMap<>();
     String collection, category;
+    Spinner category_spinner;
+    String[] cateList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +42,65 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         collection = getIntent().getStringExtra("collection");
         category = getIntent().getStringExtra("category");
-        String[] cateList = new String[]{category};
+        cateList = new String[]{category};
 
         postHelper = new HelpPosting();
         listView = (ListView) findViewById(R.id.search_listview);
+        category_spinner = findViewById(R.id.search_cate_spinner);
 
+        int i = 0;
+        for(String str :getResources().getStringArray(R.array.category)){
+            if(str.equals(category)) break;
+            i++;
+        }
+
+        category_spinner.setSelection(i);
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("postID", adapter.getItem(position).getPostID());
+                intent.putExtra("collection", collection);
+                startActivity(intent);
+            }
+        });
+
+        category_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    category = null;
+                } else {
+                    category = getResources().getStringArray(R.array.category)[position];
+                    cateList = new String[]{category};
+                }
+                showList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.android),
+//                "안드로이드", "5", "~2021/5/31", "3", "5");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.java),
+//                "자바", "6", "~2021/5/31", "4", "6");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.kotlin),
+//                "코틀린", "3", "~2021/5/31", "1", "0");
+//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.python),
+//                "파이썬", "7", "~2021/5/31", "2", "2");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void showList() {
         adapter = new PostListAdapter(collection);
-        listView.setAdapter(adapter);
-
         if (collection.equals(HelpPosting.PROJECT)) { // Project
             if (category == null) { // 전체보기인 경우
                 postHelper.getAllPosts(HelpPosting.PROJECT).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -190,31 +244,5 @@ public class SearchActivity extends AppCompatActivity {
                 });
             }
         }
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
-                intent.putExtra("postID", adapter.getItem(position).getPostID());
-                intent.putExtra("collection", collection);
-                startActivity(intent);
-            }
-        });
-
-
-//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.android),
-//                "안드로이드", "5", "~2021/5/31", "3", "5");
-//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.java),
-//                "자바", "6", "~2021/5/31", "4", "6");
-//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.kotlin),
-//                "코틀린", "3", "~2021/5/31", "1", "0");
-//        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), R.drawable.python),
-//                "파이썬", "7", "~2021/5/31", "2", "2");
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 }
